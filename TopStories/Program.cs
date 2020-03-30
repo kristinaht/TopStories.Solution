df77e16d4420b2535cb34271e8d6450128539612
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-30 15:47:00 -0700

    modify Program Main class to display nicely formatted response using DeserializeObject<List<Article>> ...

--- TopStories/Program.cs
+++ TopStories/Program.cs
@@ -3,20 +3,40 @@ using System.Threading.Tasks;
 using RestSharp;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq; //this is where JObject comes from. JObject is .NET object that can be treated as JSON
+using System.Collections.Generic;
+using ConsoleApiCall;
 
 namespace ApiTest
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
-      var apiCallTask = ApiHelper.ApiCall("Ws7xotfERIVKDMORGlCVhlmsfcoy8xKB");//this variable takes the returned Task from the async function in the ApiHelper class below. .We than call the ApiCall method and pass our api key in
-
-      //using code below we turn giant string stored as result into JSON data.
+      var apiCallTask = ApiHelper.ApiCall("API KEY HERE");
       var result = apiCallTask.Result;
-      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result); //this is where the conversion happens.
-      Console.WriteLine(jsonResponse["results"]);
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+      foreach (Article article in articleList)
+      {
+        Console.WriteLine($"Section: {article.Section}");
+        Console.WriteLine($"Title: {article.Title}");
+        Console.WriteLine($"Abstract: {article.Abstract}");
+        Console.WriteLine($"Url: {article.Url}");
+        Console.WriteLine($"Buline: {article.Byline}");
+      }
     }
+    //CODE BELOW WAS USED TO SHOW RESULT IN CONSOLE AS API RESULT
+    // static void Main(string[] args)
+    // {
+    //   var apiCallTask = ApiHelper.ApiCall("Ws7xotfERIVKDMORGlCVhlmsfcoy8xKB");//this variable takes the returned Task from the async function in the ApiHelper class below. .We than call the ApiCall method and pass our api key in
+
+    //   //using code below we turn giant string stored as result into JSON data.
+    //   var result = apiCallTask.Result;
+    //   JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result); //this is where the conversion happens.
+    //   Console.WriteLine(jsonResponse["results"]);
+    // }
   }
 
   class ApiHelper
