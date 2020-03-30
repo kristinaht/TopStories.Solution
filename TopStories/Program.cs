921d8a1255e94bf072626aa677c01116c12a744d
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-30 12:03:55 -0700

    modify program and csproj files to match lesson

--- TopStories/Program.cs
+++ TopStories/Program.cs
@@ -8,7 +8,7 @@ namespace ApiTest
   {
     static void Main()
     {
-      var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
+      var apiCallTask = ApiHelper.ApiCall("Ws7xotfERIVKDMORGlCVhlmsfcoy8xKB");//this variable takes the returned Task from the async function in the ApiHelper class below. .We than call the ApiCall method and pass our api key in
       var result = apiCallTask.Result;
       Console.WriteLine(result);
     }
@@ -16,12 +16,12 @@ namespace ApiTest
 
   class ApiHelper
   {
-    public static async Task<string> ApiCall(string apiKey)
+    public static async Task<string> ApiCall(string apiKey) //whenever e method is declared as async we need to return a Task type
     {
-      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2"); //we instantiate the RestSharp.RestClient object and in it we store the connection. We call this variable client.
       RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
-      var response = await client.ExecuteTaskAsync(request);
-      return response.Content;
+      var response = await client.ExecuteTaskAsync(request); //ExecuteTAskAsync is RestClient's method
+      return response.Content; //returns Content of response variable
     }
   }
 }
